ab861f357769e568a59112c1fb4b53c2a4f0c01a
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-20 12:21:10 +0000

    feat: task 3 and 4 -> created a Stack<string>

--- SuperheroAgency/Program.cs
+++ SuperheroAgency/Program.cs
@@ -1,17 +1,35 @@
-﻿namespace SuperheroAgency
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SuperheroAgency
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            var list = new List<string>() { "hello", "world"};
 
-            var dict = new Dictionary<int, string> // the key must be int, value must be string
+            int[] intArray = { 1, 5, 2, 8, 6 };
+            Console.WriteLine(GetLast(intArray)); // 6
+
+            double[] doubleArray = { 10.5, 60.5, 2.2, 8.76, 6.1111 };
+            Console.WriteLine(GetLast(doubleArray)); // 6.1111
+
+            string[] stringArray = { "hello", "world", "it's", "northcoders!" };
+            Console.WriteLine(GetLast(stringArray)); // "northcoders"
+
+            Stack<string> books = new Stack<string>();
+            books.Push("books1");
+            books.Push("books2");
+            books.Push("books3");
+            var x = books.Pop();
+            Console.WriteLine(x);
+            Console.WriteLine(books.Count);
+            foreach (var book in books)
             {
-                {1, "a"},
-                {2, "b"}
-            };
+                Console.WriteLine(book);
+            }
         }
+
+        public static T GetLast<T>(T[] array) => array[^1]; 
     }
 }
