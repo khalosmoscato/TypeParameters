53aa60db872e0341f451d1edcb51faa2ac9ec960
Yosefgid <jossykasshun@yahoo.com>
2026-03-20 15:34:08 +0000

    feat: Add abstract Mission

---
+++ SuperheroAgency/Mission.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperheroAgency
+{
+    public abstract class Mission<T> 
+    {
+        public string Location { get; private set; }
+        public int Reward { get; private set; }
+
+        public List<T> HeroesDeployed { get; } = new List<T>();
+
+        public Mission(string location, int reward)
+        {
+            Location = location;
+            Reward = reward;
+        }
+    }
+}
