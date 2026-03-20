f4f2f60ce523591c4045d2046bd0e48e3530fa12
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-20 11:18:06 +0000

    Add project files.

---
+++ TypeParameters/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TypeParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
