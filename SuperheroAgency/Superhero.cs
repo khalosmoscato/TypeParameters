1f4a2cc5bee081fbdc6c56e673a47522bc6c2987
Yosefgid <jossykasshun@yahoo.com>
2026-03-20 14:57:32 +0000

    feat: Add a Superhero class

---
+++ SuperheroAgency/Superhero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperheroAgency
+{
+    public abstract class Superhero
+    {
+        public string Alias { get; }
+        public string SecretIdentity { get; }
+        public int Age { get; }
+        public Alignment Alignment { get; }
+
+        public Superhero(string alias, string secretIdentity, int age, Alignment alignment)
+        {
+            Alias = alias;
+            SecretIdentity = secretIdentity;
+            Age = age;
+            Alignment = alignment;
+        }
+
+    }
+}
