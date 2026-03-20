a8d8c8fa26f1ab4a95193299f83c4a2f2d9d1321
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-20 11:58:31 +0000

    feat: task 2 -> created list and dictionary in Main

--- SuperheroAgency/Program.cs
+++ SuperheroAgency/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var list = new List<string>() { "hello", "world"};
+
+            var dict = new Dictionary<int, string> // the key must be int, value must be string
+            {
+                {1, "a"},
+                {2, "b"}
+            };
         }
     }
 }
