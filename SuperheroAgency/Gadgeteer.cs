e8ce917e0f693b414bd31d63fd0e1f506a9c8f0f
Yosefgid <jossykasshun@yahoo.com>
2026-03-20 15:17:40 +0000

    feat: Add 3 subclasses

---
+++ SuperheroAgency/Gadgeteer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperheroAgency
+{
+    public class Gadgeteer: Superhero, ITech, IStrength
+    {
+    }
+}
---
+++ SuperheroAgency/Tank.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperheroAgency
+{
+    public class Tank : Superhero, IFly, IStrength
+    {
+    }
+}
